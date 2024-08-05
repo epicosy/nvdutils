1b9672345fed997d8a690f34011e32646b3211ac
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-08-05 19:18:48 -0400

    script to measure platform specific cves in apps by target hardware

--- examples/measure_platform_specific_cves_by_target_hw.py
+++ examples/measure_platform_specific_cves_by_target_hw.py
@@ -11,17 +11,17 @@ loader = JSONFeedsLoader(data_path='~/.nvdutils/nvd-json-data-feeds',
 # Populate the loader with CVE records
 loader.load()
 
-tgt_sw = defaultdict(int)
+tgt_hw = defaultdict(int)
 vendor_product_pairs = set()
 app_specific_cves = 0
-has_target_sw = 0
+has_target_hw = 0
 platform_specific_cves = 0
 
 for cve_id, cve in loader.records.items():
     is_app_specific = True
     is_platform_specific = False
 
-    vendor_product_tgt_sw = defaultdict(lambda: defaultdict(int))
+    vendor_product_tgt_hw = defaultdict(lambda: defaultdict(int))
 
     for configuration in cve.configurations:
         if not is_app_specific:
@@ -39,36 +39,36 @@ for cve_id, cve in loader.records.items():
                     is_app_specific = False
                     break
 
-                if cpe_match.cpe.target_sw in ['*', '-']:
+                if cpe_match.cpe.target_hw in ['*', '-']:
                     continue
 
                 vendor_product = f"{cpe_match.cpe.vendor}::{cpe_match.cpe.product}"
-                vendor_product_tgt_sw[vendor_product][cpe_match.cpe.target_sw] += 1
+                vendor_product_tgt_hw[vendor_product][cpe_match.cpe.target_hw] += 1
 
-                if cpe_match.is_platform_specific_sw:
-                    is_platform_specific = cpe_match.is_platform_specific_sw
+                if cpe_match.is_platform_specific_hw:
+                    is_platform_specific = cpe_match.is_platform_specific_hw
 
     if not is_app_specific:
         continue
 
     app_specific_cves += 1
 
-    if len(vendor_product_tgt_sw) == 0:
+    if len(vendor_product_tgt_hw) == 0:
         continue
 
-    has_target_sw += 1
+    has_target_hw += 1
 
     if is_platform_specific:
         platform_specific_cves += 1
 
-    for vendor_product, target_sw in vendor_product_tgt_sw.items():
+    for vendor_product, target_hw in vendor_product_tgt_hw.items():
         vendor_product_pairs.add(vendor_product)
-        for sw, _ in target_sw.items():
+        for hw, _ in target_hw.items():
             # count only once per vendor-product
-            tgt_sw[sw] += 1
+            tgt_hw[hw] += 1
 
 
-print(len(vendor_product_pairs), dict(tgt_sw))
+print(len(vendor_product_pairs), dict(tgt_hw))
 print(f"App specific CVEs: {app_specific_cves}")
-print(f"CVEs with target SW: {has_target_sw}")
+print(f"CVEs with target HW: {has_target_hw}")
 print(f"Platform specific CVEs: {platform_specific_cves}")
