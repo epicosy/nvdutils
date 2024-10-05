7c54dc37d702da2f6a6dd8cdf912fe852666b5c1
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2024-10-05 15:34:07 -0400

    get weakness ids;

--- nvdutils/types/weakness.py
+++ nvdutils/types/weakness.py
@@ -19,6 +19,15 @@ class WeaknessDescription:
 
         return self.value not in ['NVD-CWE-noinfo', 'NVD-CWE-Other']
 
+    def get_numeric_value(self) -> int:
+        """
+            :return: the numeric value of the CWE ID or -1 if not a CWE ID
+        """
+        if self.is_cwe_id():
+            return int(self.value.split('-')[-1])
+
+        return -1
+
     def __str__(self):
         return f"{self.lang}: {self.value}"
 
@@ -35,6 +44,12 @@ class Weakness:
     def is_cwe_id(self, value: str = None) -> bool:
         # If True for any descriptions, then the weakness is a/the CWE ID
         return any(desc.is_cwe_id(value) for desc in self.description)
+
+    def get_numeric_values(self) -> List[int]:
+        """
+            :return: the numeric value of the CWE ID or -1 if not a CWE ID
+        """
+        return [desc.get_numeric_value() for desc in self.description]
 
     def __len__(self):
         return len(self.description)
