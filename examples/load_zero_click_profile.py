163553fde7650213d51a7ec39e73f29a4b5ce7ee
epicosy <eduard.pinconschi@tecnico.ulisboa.pt>
2025-01-23 17:44:04 +0000

    zero-click profile for CVEs with no user interaction;

--- examples/load_zero_click_profile.py
+++ examples/load_zero_click_profile.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-from nvdutils.data.profiles.basic import BasicProfile
+from nvdutils.data.profiles.zero_click import ZeroClickProfile
 from nvdutils.loaders.json.default import JSONDefaultLoader
 
-loader = JSONDefaultLoader(verbose=True, profile=BasicProfile)
+loader = JSONDefaultLoader(verbose=True, profile=ZeroClickProfile)
 
 cve_dict = loader.load(Path("~/.nvdutils/nvd-json-data-feeds"), include_subdirectories=True)
 
